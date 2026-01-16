e27ffb7e2b5f8c5fbbc3acb2bcfe151e352928bc
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-16 17:16:02 +0900

    (auth) UserModel - JsonSerializable 적용: 1, 2 - data (2, 3)

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,5 +1,10 @@
+import 'package:clean_todo_api/core/converters/converters.dart';
 import 'package:clean_todo_api/features/auth/domain/entities/user_entitiy.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'user_model.g.dart';
+
+@JsonSerializable(createToJson: false)
 class UserModel extends User {
   const UserModel({
     required super.id,
@@ -8,4 +13,53 @@ class UserModel extends User {
     required super.createdAt,
     required super.updatedAt,
   });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+
+  @JsonKey(readValue: idOrUnderscoreId)
+  @override
+  String get id;
+
+  @override
+  String get username;
+
+  @override
+  String get email;
+
+  @JsonKey(
+    fromJson: fromDateTime,
+    readValue: createdAtOrCreatedUnderscoreAt,
+  )
+  @override
+  DateTime get createdAt;
+
+  @JsonKey(
+    fromJson: fromDateTime,
+    readValue: updatedAtOrUpdatedUnderscoreAt,
+  )
+  @override
+  DateTime get updatedAt;
+
+  @override
+  String toString() {
+    return 'UserModel(id: $id, username: $username, email: $email, '
+        'createdAt: $createdAt, updatedAt: $updatedAt)';
+  }
+
+  UserModel copyWith({
+    String? id,
+    String? username,
+    String? email,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
 }
